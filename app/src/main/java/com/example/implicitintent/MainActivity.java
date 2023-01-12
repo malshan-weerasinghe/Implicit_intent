d2a1277ce6aa18ea63b6a0881f5a19178341b51a
malshan-weerasinghe <92565486+malshan-weerasinghe@users.noreply.github.com>
2023-01-12 19:18:41 +0530

    Version 1.0

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -7,41 +7,51 @@ import android.app.Notification;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-
+    EditText email,subject,message;
 
     @SuppressLint("IntentReset")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //xml references(Linking widets to java)
         button=findViewById(R.id.btn1);
-
-        Intent intent=new Intent(Intent.ACTION_SEND);
-        final Intent chooser;
-
-        intent.setData(Uri.parse("mailto:"));
-        // intent.setData(Uri.parse("tel:(+94)77123456"));//to open sms
-        //intent.setData(Uri.parse("geo:50.123,7.1434?z=19"));//to open map
-        intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"Example@gmail.com"});
-        intent.putExtra(Intent.EXTRA_SUBJECT,"This is subject");
-        intent.putExtra(Intent.EXTRA_TEXT,"This is message");
-        intent.setType("text/plain");
-        chooser = Intent.createChooser(intent,"Choose your email APP");
-
-        // intent.setData(Uri.parse("tel:(+94)77123456"));//to open sms
-        //intent.setData(Uri.parse("geo:50.123,7.1434?z=19"));//to open map
+        email=findViewById(R.id.txtEmail);
+        subject=findViewById(R.id.txtSubject);
+        message=findViewById(R.id.txtMessege);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String mail = email.getText().toString();
+                String Sub = subject.getText().toString();
+                String Msg = message.getText().toString();
+
+                Intent intent=new Intent(Intent.ACTION_SEND);//creating a intent and set a action
+                Intent chooser;
+
+                intent.setData(Uri.parse("mailto:"));//setting the data
+                intent.putExtra(Intent.EXTRA_EMAIL,new String[]{mail});//Extra Details
+                intent.putExtra(Intent.EXTRA_SUBJECT,Sub);
+                intent.putExtra(Intent.EXTRA_TEXT,Msg);
+
+                intent.setType("message/rfc822");//
+                chooser = Intent.createChooser(intent,"Choose your email APP");//to create  choosing list
+
                 startActivity(chooser);
 
             }
         });
+
+
     }
 }
